5b1c79025a18cdabfbcb880f1dc122f817141b05
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2018-02-11 18:12:35 +0100

    Added compass

--- init.h
+++ init.h
@@ -1,5 +1,7 @@
 const bool DEBUG = true;
-const bool DEBUG_SENSOR = false;
+const bool DEBUG_POTENT = false;
+const bool DEBUG_SENSOR = true;
+const bool DEBUG_COMPASS = true;
 
 const int TIME_OUT = 50000;
 
@@ -16,13 +18,17 @@ const int LED_PIN = 8;
 const int REFLECTANCE_PIN = A0;
 
 const int POTENTIOMETER_PIN = A1;
+const int POTENTIOMETER_TURN_PIN = A2;
 
 const int MAX_SPEED = 100;
 const int LOW_SPEED = 50;
 const int TURN_SPEED = 100;
-const int CORRECT_SPEED = 100;
+const int CORRECT_SPEED = 200;
 
-const int OBSTACLE_DISTANCE_SLOWDOWN = 100;
+// const int OBSTACLE_DISTANCE_SLOWDOWN = 100;
+// const int OBSTACLE_DISTANCE_STOP = 50;
+// const int OBSTACLE_DISTANCE_TURN = 40;
+const int OBSTACLE_DISTANCE_SLOWDOWN = 70;
 const int OBSTACLE_DISTANCE_STOP = 50;
 const int OBSTACLE_DISTANCE_TURN = 40;
 const int OBSTACLE_DISTANCE_SIDE_CORRECT = 20;
@@ -30,6 +36,8 @@ const int OBSTACLE_DISTANCE_SIDE_CORRECT = 20;
 const int TIME_TURN_180 = 1000;
 const int TIME_TURN_90 = 500;
 const int TIME_TURN_10 = 100;
+
+const int TURN_DEGREE = 30;
 
 const int TURN_MODE = 2; // 0: in place, 1 smooth, 2: constant (turn approx. 90 degrees each time)
 /*
@@ -42,3 +50,23 @@ const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
 
 /* Vitesse du son dans l'air en mm/us */
 const float SOUND_SPEED = 340.0 / 1000;
+
+
+/* Couleurs */
+/* Couleurs (format RGB) */
+const int COLOR_BLACK[3]  = {0, 0, 0};
+const int COLOR_RED[3]    = {255, 0, 0};
+const int COLOR_GREEN[3]  = {0, 255, 0};
+const int COLOR_BLUE[3]   = {0, 0, 255};
+const int COLOR_YELLOW[3] = {255, 255, 0};
+const int COLOR_BROWN[3]  = {160, 90, 44};
+const int COLOR_VIOLET[3] = {128, 0, 128};
+
+
+/* Broches */
+const int PIN_LED_R = 12;
+const int PIN_LED_G = 11;
+const int PIN_LED_B = 10;
+
+
+
--- robot.ino
+++ robot.ino
@@ -1,12 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include <Adafruit_APDS9960.h>
-#include <Servo.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Servo.h> 
 
 #include "init.h"
 
 
 Adafruit_MotorShield motorShield = Adafruit_MotorShield();
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
 
 // TODO
@@ -76,6 +80,49 @@ class Flasher
     }
 };
 
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+class Color
+{
+    //int _ledState;                 // ledState used to set the LED
+
+    // Constructor - creates a Flasher
+    // and initializes the member variables and state
+    public:
+    Color()
+    {
+    }
+
+    void init() {
+        pinMode(PIN_LED_R, OUTPUT);
+        pinMode(PIN_LED_G, OUTPUT);
+        pinMode(PIN_LED_B, OUTPUT);
+        //displayColor(0, 255, 0);
+        this->displayColor(COLOR_BLUE);
+        delay(100);
+        this->displayColor(COLOR_RED);
+        delay(100);
+        this->displayColor(COLOR_GREEN);
+        delay(100);
+        this->displayColor(COLOR_VIOLET);
+        delay(100);
+        this->displayColor(COLOR_YELLOW);
+        delay(100);
+        this->displayColor(COLOR_BROWN);
+        delay(100);
+    }
+
+    //void displayColor(byte r, byte g, byte b) {
+    void displayColor(int c[]) {
+        // Assigne l'état des broches
+        // Version anode commune
+        analogWrite(PIN_LED_R, 255 - c[0]);
+        analogWrite(PIN_LED_G, 255 - c[1]);
+        analogWrite(PIN_LED_B, 255 - c[2]);
+    }
+
+
+};
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -289,6 +336,79 @@ class Motor
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
+class Compass
+{
+    int _motorPin;
+    Adafruit_DCMotor *_motor;
+
+    // Constructor - creates a Sound
+    // and initializes the member variables and state
+    public:
+    Compass()
+    {
+    }
+
+    void init() {
+        if(!mag.begin())
+        {
+            if(DEBUG_COMPASS)
+                Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+            while(1);
+        }
+    }
+
+    float getHeading() {
+        sensors_event_t event; 
+        mag.getEvent(&event);
+
+        float Pi = 3.14159;
+
+        float tabHeadings[5];
+
+//         for(int i = 0; i < 5; i++) {
+            // Calculate the angle of the vector y,x
+
+            // calibration    
+            //                  x       y      z
+            //  Mag Minimums: -47.82  -70.64  -69.29
+            //  Mag Maximums: 58.27  36.36  44.18      
+            float avgX = (-47.82 + 58.27) / 2;
+            float avgY = (-70.64 + 36.36) / 2;
+            
+            float deltaX = event.magnetic.x - avgX;
+            float deltaY = event.magnetic.y - avgY;
+            
+            //float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+            float heading = (atan2(deltaY ,deltaX) * 180) / Pi;
+
+            // Normalize to 0-360
+            if (heading < 0)
+            {
+                heading = 360 + heading;
+            }
+
+            //tabHeadings[i] = heading;
+
+            if(DEBUG_COMPASS) {
+//                 Serial.print("  Heading: ");
+//                 Serial.println(heading);
+            }
+
+            // TODO 
+            // Il y a des fois des valeurs absurdes
+            // Il faut donc prendre 5 mesures et prendre  celle qui est la plus différentes des autres ,et renvoyer une moyenne
+            // ex:  5 6 5 300 5 => on enelève le 300
+//         }
+        
+        // TODO avg tabHeadings
+        
+        return heading;
+    }
+
+};
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 class Movement
 {
     int _speed = MAX_SPEED;
@@ -298,6 +418,7 @@ class Movement
     int _turnMode;
     int _turn180Time;
     int _turn90Time;
+    int _turnDegTime;
     int _direction;
     float _distanceFront;
     float _distanceLeft;
@@ -306,14 +427,21 @@ class Movement
     Motor *_motorBackLeft;
     Motor *_motorFrontRight;
     Motor *_motorBackRight;
+    UltraSound *_sensorFront;
+    Color *_color;
+    Compass *_compass;
 
     public:
-    Movement(Motor *mFrontLeft, Motor *mBackLeft, Motor *mFrontRight, Motor *mBackRight)
+    Movement(Compass *mCompass, Motor *mFrontLeft, Motor *mBackLeft, Motor *mFrontRight, Motor *mBackRight, UltraSound *mSensorFront, Color *mColor)
     {
+        this->_compass = mCompass;
+
         this->_motorFrontLeft = mFrontLeft;
         this->_motorBackLeft = mBackLeft;
         this->_motorFrontRight = mFrontRight;
         this->_motorBackRight = mBackRight;
+        this->_sensorFront = mSensorFront;
+        this->_color = mColor;
 
         this->_maxSpeed = MAX_SPEED;
         this->_lowSpeed = LOW_SPEED;
@@ -332,9 +460,11 @@ class Movement
         this->_maxSpeed = speed;
     }
 
+    //void setTurnSpeed(int speed, int time)
     void setTurnSpeed(int speed)
     {
         this->_turnSpeed = speed;
+        //this->setTurnTime(speed, time);
         this->setTurnTime(speed);
     }
 
@@ -354,21 +484,27 @@ class Movement
         this->_turnMode = turnMode;
     }
 
+    //void setTurnTime(int speed, int time) {
     void setTurnTime(int speed) {
         this->_turn180Time = map(speed, 0, 255, 500, TIME_TURN_180);
         this->_turn90Time = map(speed, 0, 255, 200, TIME_TURN_90);
+        //this->_turnDegTime = time;
+        this->_turnDegTime = this->_turn90Time;
     }
 
     int update() {
         // According to the distanceFront, slow down, and turn (if distanceLeft and distanceRight are OK)
         if (this->_distanceFront > OBSTACLE_DISTANCE_STOP) {
+            _color->displayColor(COLOR_GREEN);
             this->_direction = 90;
             if (this->_distanceFront > OBSTACLE_DISTANCE_SLOWDOWN) {
                 this->setSpeed(this->_maxSpeed);
                 this->forward();
                 this->_direction = 90;
                 if (DEBUG) {
-                    Serial.println("FORWARD FULL SPEED");
+                    //Serial.println("FORWARD FULL SPEED");
+                    Serial.print("FORWARD FULL SPEED     ");
+                    Serial.println(_compass->getHeading());
                 }
             } else if (this->_distanceFront > OBSTACLE_DISTANCE_STOP) {
                 int valeur = map(this->_distanceFront, 10, 100, this->_lowSpeed, this->_maxSpeed); // Conversion en valeur
@@ -389,6 +525,7 @@ class Movement
             // SOLUTION => detect that case (if there are too many left turn / right turn / left turn / right turn...then force a local loop to procede a 180 degree (according to the current speed, make some tests to determine the necessary time to do that)
             if((this->_distanceLeft < this->_distanceRight) && (this->_distanceRight > OBSTACLE_DISTANCE_TURN)) {
                 // we have to turn right until the FRONT is clear (_distanceFront > OBSTACLE_DISTANCE_TURN), and the LEFT is away suffisant from the right obstacle (_distanceLeft >    OBSTACLE_DISTANCE_TURN)
+                _color->displayColor(COLOR_YELLOW);
                 // But i think it will be done by the correct function
                 if (DEBUG) {
                     Serial.println("TURN RIGHT");
@@ -399,6 +536,7 @@ class Movement
             else if((this->_distanceLeft > this->_distanceRight) && (this->_distanceLeft > OBSTACLE_DISTANCE_TURN)) {
                 // we have to turn left until the FRONT is clear (_distanceFront > OBSTACLE_DISTANCE_TURN), and the RIGHT is away suffisant from the left obstacle (_distanceRight > OBSTACLE_DISTANCE_TURN)
                 // But i think it will be done by the correct function
+                _color->displayColor(COLOR_BLUE);
                 if (DEBUG) {
                     Serial.println("TURN LEFT");
                 }
@@ -406,7 +544,8 @@ class Movement
                 this->_direction = 0;
             } else {
                 // Do 180 turn
-                this->turn180();
+                _color->displayColor(COLOR_RED);
+                this->turnDeg(0, 180); // TODO variable for this value
                 this->_direction = 90;
             }
         }
@@ -441,7 +580,8 @@ class Movement
             this->_motorFrontRight->forward(this->_turnSpeed);
             this->_motorBackRight->forward(this->_turnSpeed);
         } else {
-            this->turn90(0);
+            //this->turnDeg(0, this->_turnDegTime);
+            this->turnDeg(0, TURN_DEGREE); // TODO variable for this value
         }
     }
 
@@ -458,73 +598,133 @@ class Movement
             this->_motorFrontRight->forward(this->_turnSpeed / 2);
             this->_motorBackRight->forward(this->_turnSpeed / 2);
         } else {
-            this->turn90(1);
+            //this->turnDeg(1, this->_turnDegTime);
+            this->turnDeg(1, TURN_DEGREE); // variable for this value
         }
     }
 
-    void turn90(int way) { // way = 0: left, 1: right
-        unsigned long previousMillis;
-        unsigned long currentMillis = millis();
-        previousMillis = currentMillis;
+    void turnDeg(int way, int deg) { // way = 0: left, 1: right, deg is given in millisecond (time to turn)
+        float startHeading = _compass->getHeading();
+        float currentHeading = startHeading;
+        //float deltaDeg = abs(currentHeading - startHeading);
+        float deltaDeg = 0.0;
+        float target = 0.0;
+        if(way) {
+            // RIGHT
+            target = currentHeading + deg;
+            if(target > 360)
+                target -= 360;
+        }
+        else {
+            // LEFT
+            target = currentHeading - deg;
+            if(target < 0)
+                target += 360;
+        }
 
-        while((currentMillis - previousMillis) <= this->_turn90Time)
+        //while(deltaDeg <= deg)
+        //while(currentHeading >= target)
+        //while((target - currentHeading) >= 0)
+        while(1)
         {
-            if (DEBUG) {
-                Serial.println("TURNING 90");
+            // Check if th front is clear, if so, stop the turn, and go
+            float distanceFront = _sensorFront->getDistance();
+            if (distanceFront > OBSTACLE_DISTANCE_STOP && deg != 180) {  // If 180, we force the halfspin
+                break;
             }
-            currentMillis = millis();
+
+            currentHeading = _compass->getHeading();
+            if (DEBUG) {
+                if(way == 0)
+                    Serial.print("TURNING LEFT: ");
+                else
+                    Serial.print("TURNING RIGHT: ");
+                Serial.print(deg);
+                Serial.print("                  TARGET: ");
+                Serial.print(target);
+                Serial.print("    CURRENT: ");
+                Serial.println(currentHeading);
+            }
+
             if(way) {
+                // RIGHT
+//                 deltaDeg = startHeading + currentHeading;
+                if(currentHeading > 360)
+                    currentHeading -= 360;
+                if(currentHeading > target)
+                    break;
                 this->_motorFrontLeft->forward(this->_turnSpeed);
                 this->_motorBackLeft->forward(this->_turnSpeed);
                 this->_motorFrontRight->backward(this->_turnSpeed);
                 this->_motorBackRight->backward(this->_turnSpeed);
             }
             else {
+                // LEFT
+//                 deltaDeg = currentHeading - startHeading;
+                if(currentHeading < 0)
+                    currentHeading += 360;
+                if(currentHeading < target)
+                    break;
                 this->_motorFrontLeft->backward(this->_turnSpeed);
                 this->_motorBackLeft->backward(this->_turnSpeed);
                 this->_motorFrontRight->forward(this->_turnSpeed);
                 this->_motorBackRight->forward(this->_turnSpeed);
             }
+
+            /*if (DEBUG) {
+                Serial.print("                              ");
+                Serial.println(deltaDeg);
+            }*/
         }
     }
-
+    /*
     void turn180() {
-        unsigned long previousMillis;
-        unsigned long currentMillis = millis();
-        previousMillis = currentMillis;
 
-        while((currentMillis - previousMillis) <= this->_turn180Time)
+        float startHeading = _compass->getHeading();
+        float currentHeading = startHeading;
+        float deltaDeg = abs(currentHeading - startHeading);
+
+        while(deltaDeg <= 180)
         {
             if (DEBUG) {
                 Serial.println("TURNING 180");
             }
-            currentMillis = millis();
+            currentHeading = _compass->getHeading();
+
+            currentHeading = _compass->getHeading();
+            deltaDeg = abs(currentHeading - startHeading);
+
             this->_motorFrontLeft->backward(this->_turnSpeed);
             this->_motorBackLeft->backward(this->_turnSpeed);
             this->_motorFrontRight->forward(this->_turnSpeed);
             this->_motorBackRight->forward(this->_turnSpeed);
         }
     }
+    */
 
     void correctLeft() {
         // If an obstacle get closer on the LEFT, then slow down the RIGHT motors to correct the direction
         if (this->_distanceLeft < OBSTACLE_DISTANCE_SIDE_CORRECT) {
+            _color->displayColor(COLOR_VIOLET);
             if (DEBUG) {
-                Serial.println("FORWARD: correcting LEFT");
+                Serial.print("   correcting LEFT  ");
             }
             this->_motorFrontRight->backward(CORRECT_SPEED);
             this->_motorBackRight->backward(CORRECT_SPEED);
+            //this->turnDeg(0, 10); // variable for this value
         }
     }
 
     void correctRight() {
         // If an obstacle get closer on the RIGHT, then slow down the LEFT motors to corect the direction
         if (this->_distanceRight < OBSTACLE_DISTANCE_SIDE_CORRECT) {
+            _color->displayColor(COLOR_BROWN);
              if (DEBUG) {
-                 Serial.println("FORWARD: correcting RIGHT");
+                Serial.print("   correcting RIGHT  ");
              }
-            this->_motorFrontLeft->backward(CORRECT_SPEED);
-            this->_motorBackLeft->backward(CORRECT_SPEED);
+             this->_motorFrontLeft->backward(CORRECT_SPEED);
+             this->_motorBackLeft->backward(CORRECT_SPEED);
+            //this->turnDeg(1, 10); // variable for this value
         }
     }
 
@@ -537,30 +737,6 @@ class Movement
 };
 
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-/*
-class ServoControl
-{
-    Servo *_servo;
-
-    // Constructor - creates a Sound
-    // and initializes the member variables and state
-    public:
-    ServoControl(Servo *servo, int servoPin)
-    {
-        _servo = servo;
-        _servo->attach(servoPin);
-        _servo->write(90);
-    }
-
-    void update(int pos) {
-        if(pos < 0 or pos > 180)
-            return;
-        _servo->write(pos);
-    }
-};
-*/
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
@@ -578,9 +754,17 @@ Motor motorBackLeft(2);
 Motor motorFrontRight(4);
 Motor motorBackRight(3);
 
-Movement m(&motorFrontLeft, &motorBackLeft, &motorFrontRight, &motorBackRight);
+Compass compass;
 
-Servo servo;
+Color color;
+
+Movement m(&compass, &motorFrontLeft, &motorBackLeft, &motorFrontRight, &motorBackRight, &sensorFront, &color);
+
+//Servo servo;
+
+
+
+
 //ServoControl servoControl(&servo, 10);
 
 
@@ -591,11 +775,19 @@ int turnMode = TURN_MODE;
 void setup() {
     if (DEBUG)
         Serial.begin(9600);
+    if (DEBUG_POTENT)
+        Serial.begin(9600);
+    if(DEBUG_COMPASS)
+        Serial.begin(9600);
     //gestureSensor.init();
     //colorSensor.init();
 
     //servo.attach(10);
     motorShield.begin(); //On lance la communication avec le shield
+
+    compass.init();
+
+    color.init();
 }
 
 
@@ -614,9 +806,21 @@ void loop() {
 
     //reflectanceSensor.getReflectance();
 
-
     int valPoten = analogRead(POTENTIOMETER_PIN);
+    if(DEBUG_POTENT) {
+        Serial.print("Speed: ");
+        Serial.println(valPoten);
+    }
     int newSpeed = map(valPoten, 0, 1023, 0, 255);
+
+    //color.displayColor(newSpeed, 0, newSpeed);
+
+    /*int valTurnPoten = analogRead(POTENTIOMETER_TURN_PIN);
+    int newTurnTime = map(valTurnPoten, 0, 1023, 0, 1000);
+    if(DEBUG_POTENT) {
+        Serial.print("Turn: ");
+        Serial.println(newTurnTime);
+    }*/
 
     // Stop the whole system if TIME_OUT is reached
     unsigned long currentMillis = millis();
@@ -633,6 +837,7 @@ void loop() {
 
     if(newSpeed <= 255) {
         m.setMaxSpeed(newSpeed);
+        //m.setTurnSpeed(newSpeed, newTurnTime);
         m.setTurnSpeed(newSpeed);
     }
 
@@ -648,6 +853,9 @@ void loop() {
     }
     //led1.update(distanceFront);
     //colorSensor.getColor();
+    
+
+    
 }
 
 
